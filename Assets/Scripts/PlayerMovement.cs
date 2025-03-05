11d9300bc29016fc2754c8eb5ec34ad0e15d40ae
alberto <betomiku@gmail.com>
2025-03-05 16:27:26 +0100

    Add spaceship movement.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,12 @@ using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private Rigidbody playerRigidbody;
+    [SerializeField] private float forceMagnitude = 10f;
+    [SerializeField] private float maxSpeed = 500f;
     private Camera mainCamera;
+    private Vector2 movementDirection;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -34,9 +39,36 @@ public class PlayerMovement : MonoBehaviour
         return touchPositionsSum;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateMovementDirection()
     {
-        
+        movementDirection =  (TouchWorldPosition() - playerRigidbody.transform.position).normalized;
+    }
+
+    private void Update()
+    {
+        if (Touch.activeTouches.Count > 0)
+        {
+            UpdateMovementDirection();
+        }
+    }
+
+    // We use FixedUpdate for physics calculations
+    private void FixedUpdate()
+    {
+        AddForceToRigidBody();
+        LimitSpeed();
+    }
+
+    private void AddForceToRigidBody()
+    {
+        playerRigidbody.AddForce(movementDirection * forceMagnitude, ForceMode.Force);
+    }
+
+    private void LimitSpeed()
+    {
+        if (playerRigidbody.linearVelocity.magnitude > maxSpeed)
+        {
+            playerRigidbody.linearVelocity = playerRigidbody.linearVelocity.normalized * maxSpeed;
+        }
     }
 }
