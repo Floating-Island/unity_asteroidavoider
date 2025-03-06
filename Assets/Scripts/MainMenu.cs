6026c391d8263fce5ebb0bd97fb389960a9aedb9
alberto <betomiku@gmail.com>
2025-03-06 15:53:32 +0100

    Add main menu start game logic.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
