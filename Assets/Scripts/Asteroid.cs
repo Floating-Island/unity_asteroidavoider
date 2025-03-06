9932cc49338b18253289a8c68bf75fac74e545d2
alberto <betomiku@gmail.com>
2025-03-06 14:41:28 +0100

    Refactor asteroid offscreen destruction.

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -2,13 +2,6 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    private Camera mainCamera;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        mainCamera = Camera.main;
-    }
-    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -16,18 +9,8 @@ public class Asteroid : MonoBehaviour
             other.GetComponent<PlayerHealth>().Crash();
         }
     }
-    private void Update()
+    private void OnBecameInvisible()
     {
-        DestroyOnOffScreen();
-    }
-
-    private void DestroyOnOffScreen()
-    {
-        Vector3 viewportPosition = mainCamera.WorldToViewportPoint(transform.position);
-
-        if (viewportPosition.x > 1 || viewportPosition.x < 0 || viewportPosition.y > 1 || viewportPosition.y < 0)
-        {
-            Destroy(gameObject);
-        }
+        Destroy(gameObject);
     }
 }
